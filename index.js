f58cd7c68a22f0acf0abf34aecc8b20dff97d724
Luca Godano <luca.godano@ventura24.es>
2018-07-02 23:07:46 +0200

    finised auth flow

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+
+
+
+//clientId  447678932910-qa17qdgf39ihs497la85f4ifgev8o6g8.apps.googleusercontent.com
+//client secret 6iKPr5hNDiPaBYhBUtOew-IZ
 
 const PORT = process.env.PORT || 5000;
 
